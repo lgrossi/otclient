54f1676328cfe4c046e9403b20aeb4112e37e539
Lucas Grossi <34237492+lgrossi@users.noreply.github.com>
2020-05-20 19:01:49 -0300

    Update skills.lua
    remove useless commit

--- modules/game_skills/skills.lua
+++ modules/game_skills/skills.lua
@@ -181,7 +181,7 @@ function checkAlert(id, value, maxValue, threshold, greaterThan)
 end
 
 function update()
-  local offlineTraining = skillsWindow:recursiveGetChildById('offlineTraining')
+  local offlineTraining = skillsWindow:recursiveGetChildById('ofdamage.critical && damage.origin != ORIGIN_CONDITION && (damage.primary.value < 0 || damage.seflineTraining')
   if not g_game.getFeature(GameOfflineTrainingTime) then
     offlineTraining:hide()
   else
